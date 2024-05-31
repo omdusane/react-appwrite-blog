a6ed82d826dbb486e714daf8ed3ea6125a96bc49
Om Dusane <omdusane8@gmail.com>
2024-06-01 05:02:44 +0530

    Added Basic redux workflow for auth

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,39 @@
-
-import './App.css'
+import { useEffect, useState } from "react"
+import { useDispatch } from "react-redux"
+import authService from './appwrite/auth'
+import { login, logout } from './store/authSlice'
+import { Header, Footer } from './components'
+import { Outlet } from 'react-router-dom'
 
 function App() {
-  console.log(import.meta.env.VITE_APPWRITE_URL);
+  const [loading, setLoading] = useState(true)
+  const dispatch = useDispatch()
 
-  return (
-    <>
-      <h1>Hello World</h1>
-    </>
-  )
+  useEffect(()=>{
+    authService.getCurrentUser()
+    .then((userData) => {
+      if(userData){
+        dispatch(login({userData}))
+        setLoading(false)
+      }else{
+      dispatch(logout())
+      setLoading(false)
+      }
+
+    })
+  }, [])
+
+  return loading ? (
+    <div className="min-h-screen flex flex-wrap content-between bg-grey-400">
+      <div className="w-full block">
+           <Header />
+           <main>
+            TODO{/* {<Outlet />} */}
+           </main>
+           <Footer />
+      </div>
+    </div>
+  ) : null
 }
 
 export default App
